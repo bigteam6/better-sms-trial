e5fbe64af317ab10498d8ec2e2894638b32cc87f
Nabil Freeman <nabil@matchchat.co.uk>
2014-01-08 13:22:01 +0000

    fresh

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-var entries = ["bookings","sales","donations"];
+var entries = ["bookings","sales","users","donations","audience","subscribers"];
 var entrypos = 0;
 var stringpos = 0;
 
 function loop(string, callback){
-	console.log("hey");
-
 	if(stringpos == string.length){
 		callback();
 		return;
@@ -20,6 +18,7 @@ function loop(string, callback){
 
 function cycle(instantly){
 	if(instantly){
+		$(".graphcaption").html(entries[entrypos]);
 		loop(entries[entrypos], function(){
 			cycle();
 		});
@@ -30,6 +29,7 @@ function cycle(instantly){
 		
 		setTimeout(function(){
 			$(".subject").html("");
+			$(".graphcaption").html(entries[entrypos]);
 			loop(entries[entrypos], function(){
 				cycle();
 			});
@@ -38,3 +38,38 @@ function cycle(instantly){
 }
 
 cycle(true);
+
+function animation(){
+	var obj = $(".mac .window");
+	var scrollheight = obj[0].scrollHeight;
+	var midway = scrollheight/2;
+
+	$(".cnvrt").fadeIn(400, function(){
+		$(".cnvrt").addClass("pulsate");
+		obj.animate({scrollTop: scrollheight,opacity:0.2}, 1400, function(){
+			obj.find("button").addClass("pulsate");
+			setTimeout(function(){
+				obj.animate({scrollTop:0,opacity:1},700);
+
+				$(".mouse").fadeIn(200).animate({top:275,left:520},1400, function(){
+					setTimeout(function(){
+						obj.find("button").removeClass("pulsate").addClass("active");
+						setTimeout(function(){
+							obj.find("button").removeClass("active");
+							$(".mouse").fadeOut(1000, function(){
+								$(".mouse").css({
+									"top":"",
+									"left":"",
+								});
+								setTimeout(animation, 1000);
+							});
+							$(".cnvrt").fadeOut(1000).removeClass("pulsate");
+						},1000);
+					}, 1600);
+				});
+			}, 4000);
+		});
+	});
+}
+
+setTimeout(animation, 1000);
